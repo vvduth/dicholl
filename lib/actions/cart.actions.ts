120e398f20804c056d1303a4818ca7cbdc86872c
vvduth <ducthai060501@gmail.com>
2025-02-20 08:54:21 +0200

    Add error handling and session cart ID check in addToCart function

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,8 +1,24 @@
 "use server"
 import { CartItem } from "@/types"
+import { formatError } from "../utils"
+import { cookies } from "next/headers"
 export async function addToCart(item: CartItem) {
-    return  {
-        success: true, 
-        message: "Item added to cart"
+    try {
+
+        // check for the cart cookie
+        const sessioncartId = (await cookies()).get('sessionCartId')?.value
+
+        // Testing
+        console.log(sessioncartId)
+
+        return  {
+            success: true, 
+            message: "Item added to cart"
+        } 
+    } catch (error) {
+        return {
+            success: false,
+            message: formatError(error)
+        }
     }
 }
