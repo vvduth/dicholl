48072759d6de6bc71ba640707774997fbb5333a7
vvduth <ducthai060501@gmail.com>
2025-02-21 08:53:48 +0200

    Add remote image patterns to Next.js configuration for external asset loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'as2.ftcdn.net',
+      port: '',
+      pathname: '**'
+    }, {
+      protocol: 'https',
+      hostname: 'as1.ftcdn.net',
+      port: '',
+      pathname: '**'
+    }]
+  },
 };
 
 export default nextConfig;
