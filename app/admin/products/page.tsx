5f671e6e2fd313defa506fac5b60225c1bc375ab
vvduth <ducthai060501@gmail.com>
2025-02-25 10:55:46 +0200

    Add admin products management page; implement product listing, deletion, and pagination

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import Link from "next/link";
+import { getAllProducts } from "@/lib/actions/product.action";
+import { formatCurrency, formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import DeleteDialog from "@/components/shared/delete-dialog";
+import { deleteProduct } from "@/lib/actions/product.action";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Pagination from "@/components/shared/pagination";
+const AdminProductpage = async (props: {
+  searchParams: Promise<{
+    page: string;
+    query: string;
+    category: string;
+  }>;
+}) => {
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams.page) || 1;
+  const searchtext = searchParams.query || "";
+  const category = searchParams.category || "";
+
+  const products = await getAllProducts({
+    query: searchtext || "",
+    page,
+    category,
+  });
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <h1 className="h2-bold">Products</h1>
+        <Button asChild variant={"default"}>
+          <Link href="/admin/products/create">Create product</Link>
+        </Button>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>category</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead>Rating</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.data.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{formatId(product.id)}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{formatCurrency(product.price)}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              <TableCell>{product.rating}</TableCell>
+              <TableCell className="flex gap-1">
+                <Button asChild variant={"outline"} size={"sm"}>
+                  <Link href={`/admin/products/${product.id}`}>Edit</Link>
+                </Button>
+                {/* delete */}
+                <DeleteDialog 
+                  id={product.id}
+                  action={deleteProduct}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {products?.totalPages && products.totalPages > 1 && (
+        <Pagination 
+            page={page}
+            totalPages={products.totalPages}
+        />
+      )}
+    </div>
+  );
+};
+
+export default AdminProductpage;
--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,8 +1,10 @@
 "use server"
-
+import {  formatError } from '@/lib/utils';
 import { covertoPlainObj } from "../utils";
-import { PRODUCT_LIMIT } from "../constants";
+import { PAGE_SIZE, PRODUCT_LIMIT } from "../constants";
 import { prisma } from "@/db/prisma";
+import { revalidatePath } from 'next/cache';
+
 
 // Get lates products 
 export async function getLatestProducts() {
@@ -24,4 +26,59 @@ export async function getProductBySlug(slug: string) {
             slug
         }
     })
+}
+
+
+// get all products admin
+export async function getAllProducts({
+    query, 
+    limit = PAGE_SIZE,
+    page, category
+}: {
+    query: string;
+    limit?: number;
+    page: number;
+    category?: string;
+}) {
+    const data = await prisma.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+    })
+
+    const dataCount = await prisma.product.count();
+    return {
+        data,
+        totalPages: Math.ceil(dataCount / limit),
+    }
+}
+
+export async function deleteProduct(id:string) {
+    try {
+        const productExist = await prisma.product.findFirst({
+            where: {
+                id
+            }
+        })
+
+        if (!productExist) {
+            throw new Error("Product not found")
+        }
+
+        await prisma.product.delete({
+            where: {
+                id 
+            }
+        })
+
+        revalidatePath('/admin/products')
+        return {
+            success: true,
+            message: "Product deleted successfully"
+        }
+    } catch (error) {
+        return {
+            success: false,
+            message: formatError(error)
+        }
+    }
 }
